161c05cd9eac301f8e18ca5d740e0c02267aa67f
Umka <rudstanislav1990@gmail.com>
2019-07-30 11:11:42 +0300

    copy on work PC

--- CV_v2/script.js
+++ CV_v2/script.js
@@ -8,20 +8,22 @@ var e = document.getElementById("Education");
 var f = document.getElementById("Additional_information");
 
 var obj = {
-    Objective:"I want to change the direction of my life and start a career as a junior Front-end developer in " +
-        "company and maybe in 5 years grow to the team leader",
-    Summary: "I have more than 3 years of experience with hosts on platforms: ESXI, Hyper-V, Proxmox, about 1.5 years" +
-        " of deployment of web servers on apache24, ngnix, ISS, and their support. Introduced jira CRM system to support" +
-        " users by developers. Launched corporate mail, ip-telephony. Physical servers of the company were transferred" +
-        " to virtual ones. Configured the monitoring system. Debugged the servers.\n" +
-        "In my spare time I create one-page sites according to layouts. And I constantly improve my skills in site development.\n" +
-        "I have an understanding and a small experience of working with assemblers Gulp, Webpack, Grunt.\n" +
-        "Have experience with Photoshop.",
+    Objective:"I want to change my professional life, starting a career as a Junior Front-End Developer in company, " +
+        "and maybe in 5 years grow to the team leader",
+    Summary: "I have more than 3 years of experience working with hosts on such platforms as: ESXI, Hyper-V, Proxmox;" +
+        " about 1.5 years of experience deploying web servers on Apache24, Ngnix, ISS, and their support." +
+        " Introduced Jira CRM system to provide user support from developers. Launched a corporate mail, " +
+        "IP telephony. Physical servers of the company were transferred to virtual ones. Configured the monitoring " +
+        "system. Debugged the servers. Worked with servers as Supermicro, Dell, IBM, Cloud QNAP.\n" +
+        "In my spare time, I create single-page websites according to layouts. Also, I constantly improve my skills" +
+        " in website development.\n" +
+        "I have an understanding and basic experience working with such assemblers as Gulp, Webpack, Grunt.\n" +
+        "Have experience with Adobe Photoshop",
     Experience: "(2018 - today) System Administrator, GROUP OF COMPANIES «FACTOR»\n" +
         "\n" +
         " - Server management, workstations, routers, switches and existing network applications.\n" +
         " - Monitoring and daily system checks.\n" +
-        " - Diagnostics and solution of all technical problems related to hardware and software.\n" +
+        " - Diagnostics and resolving various technical issues related to hardware and software.\n" +
         " - Support for all network systems LAN, WAN.\n" +
         " - Ensuring network security and integrity.\n" +
         " - Backup, data recovery in case of failures or other situations, system and data recovery.\n" +
@@ -30,11 +32,11 @@ var obj = {
         "\n" +
         " - Server management, workstations, computers, laptops, printers, routers, switches and existing network applications.\n" +
         " - Monitoring and daily system checks.\n" +
-        " - Diagnose and solve all technical problems related to hardware and software.\n" +
+        " - Diagnostics and resolving various technical issues related to hardware and software.\n" +
         " - Laying, construction, support of all network systems LAN, WAN.\n" +
         " - Ensuring security and integrity of the network.\n" +
         " - Backup, data recovery in case of failures or other situations, system and data recovery.\n" +
-        " - Opening of new branches, shops.\n" +
+        " - Opening new branches, shops.\n" +
         " - Connecting and configuring commercial equipment.\n" +
         " - Remote user support.\n" +
         " - Working with users.\n" +
@@ -50,16 +52,16 @@ var obj = {
         "\n"+
         "NT School – Front-end developer (2018)\n" +
         "HTML5/CSS3/SCSS/Flex-box/Grid CSS\n" +
-        "In practice, I made one-page websites on PSD layouts and uploaded to hosting (no js, just HTML + CSS)\n"+
+        "In practice, I made single-page websites using PSD layouts and uploaded to a server (no JS, just HTML + CSS)\n"+
         "\n"+
         "A-Level – Full Stack(2018-2019)\n"+
         "HTML5/CSS3/Flex-Box/JS/Git\n"+
-        "In practice, I made one-page websites on PSD layouts and uploaded to hosting",
-    Additional_information: "I am currently working as a system administrator, but I really want to change my profession" +
-        " and start a career as a front-end developer. In my spare time I improve my skills in development, create" +
-        " one-page sites according to the layouts. Watching JS lessons, learning English.\n" +
-        "- Quickly learning\n" +
-        "- Fulfillment of tasks in good faith\n" +
+        "In practice, I made single-page websites using PSD layouts and uploaded to a server",
+    Additional_information: "Currently I am working as a System Administrator, but I really want to change my profession" +
+        " and start a career as a Front-End Developer. In my spare time, I improve my skills in development, creating" +
+        " single-page websites according to the layouts. Watching JS lessons, learning English.\n" +
+        "- Quick learner\n" +
+        "- Good-faith task fulfillment\n" +
         "- Initiative\n" +
         "- Executive\n" +
         "- Punctual\n" +
@@ -67,12 +69,7 @@ var obj = {
         "- Responsible\n" +
         "- There is a great desire to learn new things and develop\n" +
         "\n" +
-        "Marital status: Married.\n" +
-        "\n" +
-        "Hobby: I like to pick mushrooms, walk kayaks, ride bicycles, drive a car.\n" +
-        "\n" +
-        "Languages:\n" +
-        "English pre-intermediate",
+        "Hobby: picking mushrooms, kayaking, ridingle bicycles, drivingle and tuning a car.\n"
 };
 
 var h3 = document.createElement('h3');
@@ -107,7 +104,7 @@ b.onclick = function () {
 
 };
 
-var data = ["HTML5/CSS3 (Responsive, Adaptive)", "JavaScript", "Adobe Photoshop", "Version Control (Git)", "Virtualizations (VMware Workstation, Hiper-V, Proxmox, ESXI)", "Active network equipment (MikroTik, Ubiquiti, Fortigate, HP)", "Win servers", "Linux systems", "Web Servers (apache24, nginx, IIS)", "Database (MS SQL, MySQL, PostgreSQL)", "CRM (Jira, Mantis, Bitrix24, 1C)"];
+var data = ["HTML5/CSS3 (Responsive, Adaptive)", "JavaScript", "Adobe Photoshop", "Version Control (Git)", "Virtualizations (VMware Workstation, Hiper-V, Proxmox, ESXI)", "Active network equipment (MikroTik, Ubiquiti, Fortigate, HP)", "Windows servers", "Linux systems", "Web Servers (Apache24, Nginx, IIS)", "Database (MS SQL, MySQL, PostgreSQL)", "CRM (Jira, Mantis, Bitrix24, 1C)", "Hardware (Supermicro, Dell, IBM, QNAP)"];
 
 c.onclick = function () {
     clear();
